339d3a780ed562de0de183a0668acf23eac57672
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-12-06 21:23:08 +0300

    transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+        <motion.div
+            className="box"
+            initial={{ x: 0 }}
+            animate={{ x: 300 }}
+            transition={{ duration: 2, ease: "linear"}}
+        />
+    </div>
+  );
 };
 
 export default App;
