c66807097d052b1a1b379a951f1c9b6863e91886
HIKMET KUTUK <hikmetkutuk@gmail.com>
2024-12-09 14:03:26 +0300

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { motion } from "framer-motion";
 import PulsingButton from "./components/PulsingButton";
 import BouncingLoader from "./components/BouncingLoader";
+import { useState } from "react";
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <div>
       <motion.div
         className="box"
-        initial={{ x: 500, y: 100 }}
-        animate={{ scale: [1, 2, 2, 1, 1, 2], rotate: [0, 0, 270, 270, 0, 0] }}
-        transition={{ duration: 10, ease: "linear" }}
+        //initial={{ x: 500, y: 100 }}
+        //animate={{ scale: [1, 2, 2, 1, 1, 2], rotate: [0, 0, 270, 270, 0, 0] }}
+        transition={{ duration: 1, ease: "linear" }}
+        variants={{
+          hidden: { opacity: 0, scale: 0.8 },
+          visible: { opacity: 1, scale: 1 },
+          exit: { opacity: 0, scale: 0.8 },
+        }}
+        initial="hidden"
+        animate={isVisible ? "visible" : "hidden"}
+        onAnimationComplete={() => setIsVisible(!isVisible)}
+        exit="exit"
+        onClick={() => setIsVisible(!isVisible)}
       />
       <PulsingButton />
       <BouncingLoader />
