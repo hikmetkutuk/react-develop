cc454530030443936cd1e96590624a61ad576a8d
HIKMET KUTUK <hikmetkutuk@gmail.com>
2024-12-12 15:04:16 +0300

    gestures

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <motion.div
         className="box"
-        //initial={{ x: 500, y: 100 }}
+        initial={{ x: 500, y: 100 }}
         //animate={{ scale: [1, 2, 2, 1, 1, 2], rotate: [0, 0, 270, 270, 0, 0] }}
         transition={{ duration: 1, ease: "linear" }}
         variants={{
@@ -19,11 +19,15 @@ const App = () => {
           visible: { opacity: 1, scale: 1 },
           exit: { opacity: 0, scale: 0.8 },
         }}
-        initial="hidden"
+        //initial="hidden"
         animate={isVisible ? "visible" : "hidden"}
         onAnimationComplete={() => setIsVisible(!isVisible)}
         exit="exit"
         onClick={() => setIsVisible(!isVisible)}
+        whileHover={{ scale: 1.1, rotate: 10 }}
+        whileTap={{ scale: 0.9, backgroundColor: "red" }}
+        drag
+        //dragConstraints={{ top: -50, left: -50, right: 50, bottom: 50 }}
       />
       <PulsingButton />
       <BouncingLoader />
